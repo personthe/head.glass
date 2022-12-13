b4a9af46107eb0a174c4109d53c9583933f754d0
personthe <84892109+personthe@users.noreply.github.com>
2022-12-12 21:36:28 -0500

    Add files via upload

--- HTMLJAVA.js
+++ HTMLJAVA.js
@@ -18,7 +18,7 @@ document.addEventListener('contextmenu', function(e) {
     }, 2000);
   });
 
-  var minecraftServer = document.createElement('div');
+var minecraftServer = document.createElement('div');
 minecraftServer.innerHTML = 'Minecraft server : 135.148.29.252:25569';
 minecraftServer.style.position = 'absolute';
 minecraftServer.style.bottom = '10px';
@@ -29,16 +29,19 @@ minecraftServer.style.fontSize = '10px';
 document.body.appendChild(minecraftServer);
 
 minecraftServer.addEventListener('click', function() {
+
   var textArea = document.createElement('textarea');
   textArea.value = '135.148.29.252:25569';
   document.body.appendChild(textArea);
   textArea.select();
   document.execCommand('copy');
   document.body.removeChild(textArea);
-  
-  
+  minecraftServer.innerHTML = 'SERVER IP COPIED TO CLIPBOARD!';
+  setTimeout(function() {
+    minecraftServer.innerHTML = 'Minecraft server : 135.148.29.252:25569';
+    }, 2000);
 
-});
+  });
 
 var sheep = new Audio('https://www.myinstants.com/media/sounds/minecraft-sheep2.mp3');
 minecraftServer.addEventListener('click', function() {
