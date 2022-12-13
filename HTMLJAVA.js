537f5dcfa8f426d7b900dd507772b1f10144e90e
personthe <84892109+personthe@users.noreply.github.com>
2022-12-12 21:15:50 -0500

    Add files via upload

---
+++ HTMLJAVA.js
@@ -0,0 +1,54 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.addEventListener('contextmenu', function(e) {
+    var dontDoThat = document.createElement('div');
+    dontDoThat.innerHTML = 'right clicking is for loosers';
+    dontDoThat.style.position = 'absolute';
+    dontDoThat.style.top = e.clientY + 'px';
+    dontDoThat.style.left = e.clientX + 'px';
+    dontDoThat.style.fontSize = '10px';
+    if (document.body.style.backgroundColor === 'yellow') {
+      dontDoThat.style.color = 'black';
+    } else {
+      dontDoThat.style.color = 'white';
+    }
+    document.body.appendChild(dontDoThat);
+    setTimeout(function() {
+      document.body.removeChild(dontDoThat);
+    }, 2000);
+  });
+
+  var minecraftServer = document.createElement('div');
+minecraftServer.innerHTML = 'Minecraft server : 135.148.29.252:25569';
+minecraftServer.style.position = 'absolute';
+minecraftServer.style.bottom = '10px';
+minecraftServer.style.left = '50%';
+minecraftServer.style.transform = 'translateX(-50%)';
+minecraftServer.style.color = 'white';
+minecraftServer.style.fontSize = '10px';
+document.body.appendChild(minecraftServer);
+
+minecraftServer.addEventListener('click', function() {
+  var textArea = document.createElement('textarea');
+  textArea.value = '135.148.29.252:25569';
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+  
+  
+
+});
+
+var sheep = new Audio('https://www.myinstants.com/media/sounds/minecraft-sheep2.mp3');
+minecraftServer.addEventListener('click', function() {
+  sheep.play();
+});
+  
+  document.body.style.overflow = 'hidden';
+  
+  document.body.style.userSelect = 'none';
+  
+  document.body.addEventListener('dragstart', function(e) {
+    e.preventDefault();
+  });
--- script.js
+++ script.js
@@ -3,67 +3,7 @@ import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
 import {GLTFLoader} from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js'
 import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
 import {DragControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/DragControls.js'
-
-
-
-
-
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-document.addEventListener('contextmenu', function(e) {
-    var dontDoThat = document.createElement('div');
-    dontDoThat.innerHTML = 'right clicking is for loosers';
-    dontDoThat.style.position = 'absolute';
-    dontDoThat.style.top = e.clientY + 'px';
-    dontDoThat.style.left = e.clientX + 'px';
-    dontDoThat.style.fontSize = '10px';
-    if (document.body.style.backgroundColor === 'yellow') {
-      dontDoThat.style.color = 'black';
-    } else {
-      dontDoThat.style.color = 'white';
-    }
-    document.body.appendChild(dontDoThat);
-    setTimeout(function() {
-      document.body.removeChild(dontDoThat);
-    }, 2000);
-  });
-
-  var minecraftServer = document.createElement('div');
-minecraftServer.innerHTML = 'Minecraft server : 135.148.29.252:25569';
-minecraftServer.style.position = 'absolute';
-minecraftServer.style.bottom = '10px';
-minecraftServer.style.left = '50%';
-minecraftServer.style.transform = 'translateX(-50%)';
-minecraftServer.style.color = 'white';
-minecraftServer.style.fontSize = '10px';
-document.body.appendChild(minecraftServer);
-
-minecraftServer.addEventListener('click', function() {
-  var textArea = document.createElement('textarea');
-  textArea.value = '135.148.29.252:25569';
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textArea);
-  
-  
-
-});
-
-var sheep = new Audio('https://www.myinstants.com/media/sounds/minecraft-sheep2.mp3');
-minecraftServer.addEventListener('click', function() {
-  sheep.play();
-});
-  
-  document.body.style.overflow = 'hidden';
-  
-  document.body.style.userSelect = 'none';
-  
-  document.body.addEventListener('dragstart', function(e) {
-    e.preventDefault();
-  });
-
-  
+//----------------------------------------------------------------------------------------------------  
 
 const fontLoader = new THREE.FontLoader()
 const canvas = document.querySelector('.webgl')
@@ -86,15 +26,12 @@ const m2 = new THREE.MeshBasicMaterial({color: 'red'});
 m2.visible = false
 const m4 = new THREE.MeshBasicMaterial();
 
-
 const m3 = new THREE.MeshStandardMaterial()
 m3.roughness = 0.7
 
 const textMat = new THREE.MeshBasicMaterial({color: 'white', wireframe: true})
 
 //OBJECTS
-
-
     const box1 = new THREE.BoxGeometry(.01,10,.01)
     const box1mesh = new THREE.Mesh(box1,m2)
     box1mesh.position.set(0,1.2,)
@@ -108,131 +45,83 @@ const textMat = new THREE.MeshBasicMaterial({color: 'white', wireframe: true})
     box2mesh.receiveShadow = true
     //scene.add(box2mesh)
    
-
-    let text;
-    fontLoader.load(
-        '/font/helvetiker_regular.typeface.json',
-        (font) =>
-        {
-           const textgeo = new THREE.TextBufferGeometry('GLASSHEAD',{
-            font: font,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: .1,
-            bevelEnabled: false,
-            bevelThickness: 0.03,
-            bevelSize: .02,
-            bevelOffset: 0,
-            bevelSegments: 1,
-            })
+let text;
+fontLoader.load('/font/helvetiker_regular.typeface.json',(font) =>{
+    const textgeo = new THREE.TextBufferGeometry('GLASSHEAD',{
+        font: font,
+        size: 0.5,
+        height: 0.2,
+        curveSegments: .1,
+        bevelEnabled: false,
+        })
             
-            text = new THREE.Mesh(textgeo,textMat)
-            textgeo.center()
-            scene.add(text)
-            text.position.set(0,.2,.9)
-            const size = .15
-            text.scale.set(size,size,size)
-            
-        
+    text = new THREE.Mesh(textgeo,textMat)
+        textgeo.center()
+        scene.add(text)
+        text.position.set(0,.2,.9)
+        const size = .15
+        text.scale.set(size,size,size)            
         }
-        
-    )
+)
     
+let room;
+let door;
+let pillar;
+let Room;
+let sky;
+  
+gltfloader.load('./objects/roomofstuff.gltf', function(glb){
 
-
-    
-    let room;
-    let door;
-    let pillar;
-    let Room;
-    let sky;
-
-    
-    gltfloader.load('./objects/roomofstuff.gltf', function(glb){
     room = glb.scene;
     groop1.add(room)
     Room = room.getObjectByName("Room");
     sky = room.getObjectByName("sky");
-
- 
-    
-    
-    
-
-    room.castShadow = true
-    room.reciveShadow = true
-
-
     pillar = room.getObjectByName("Pillar");
-
-    
-
-
     door = room.getObjectByName("Door");   
-    door.addEventListener('click', (leftclick) => {
-            door.rotation.set(0,3,0)
-    })
+    door.addEventListener('click', (leftclick) => {door.rotation.set(0,3,0)})
 
-    });
+});
 
+let head;
+gltfloader.load('./objects/head.gltf', function(glb){
 
-
-    let head;
-    gltfloader.load('./objects/head.gltf', function(glb){
     head = glb.scene;
-
-    //material change   
-    /*head.traverse((child) => {
-        if (child.isMesh) child.material = material;
-        });*/
-        head.castShadow = true
-    groop1.add(head)
-
+    head.castShadow = true
     head.position.copy(box1mesh.position);   
+    groop1.add(head)
     
-    });
+});
 
-    
+let TV;
+
+gltfloader.load('./objects/TV.gltf', function(glb){
+    TV = glb.scene;
+    screen = TV.getObjectByName("TVSCREEN");
+    groop1.add(TV)
+
+});
 
 
+function headspin(){
 
-    function headspin(){
-        if(head)head.rotation.y += 0.01;
+    if(head)head.rotation.y += 0.01;
+    
+}
 
+const groop1 = new THREE.Group()
+scene.add(groop1)
     
-    
-    }
-
-    
-    
-    const groop1 = new THREE.Group()
-    scene.add(groop1)
-    
-    const mouse = new THREE.Vector2()
-window.addEventListener('mousemove', (event) =>{
+const mouse = new THREE.Vector2()
+    window.addEventListener('mousemove', (event) =>{
     mouse.x = event.clientX / sizes.width * 2 -1
     mouse.y = - (event.clientX / sizes.width) * 2 + 1
-})
+    })
 
-    const raycaster = new THREE.Raycaster()
-
-
-
-
+//const raycaster = new THREE.Raycaster()
 
 //LIGHTS
-
-
-
-
-
-
-
-
-
-
 for(let i = 0; i< 3; i++){
-    const light3 = new THREE.DirectionalLight(0xffffff, .40)  
+    const light3 = new THREE.DirectionalLight(0xffffff, .45)  
     const lightHelper = new THREE.PointLightHelper(light3);
     light3.position.x = (Math.random() - 0.5 * 1)
     light3.position.z = 2.5
@@ -241,18 +130,13 @@ for(let i = 0; i< 3; i++){
 }
 
 
-
-
-
-
 // Dookie stuff
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight}
+    height: window.innerHeight
+}
 
 window.addEventListener('resize', () =>{
-
-    
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
@@ -264,7 +148,7 @@ window.addEventListener('resize', () =>{
 
 })
 
-
+//CAMERAS
 const camera = new THREE.PerspectiveCamera (35, sizes.width/sizes.height, .01, 1000)
 camera.position.set(0,2.45,4)
 camera.rotation.x = -.4
@@ -273,7 +157,6 @@ scene.add(camera)
 const camera2 = new THREE.PerspectiveCamera (35, sizes.width/sizes.height, .01, 1000)
 camera2.position.set(0,1,4)
 camera2.rotation.x = 0
-
 scene.add(camera2)
 
 const renderer = new THREE.WebGL1Renderer({
@@ -285,24 +168,14 @@ renderer.setPixelRatio (Math.min (window.devicePixelRatio, 2))
 renderer.gammaOuput - true
 renderer.shadowMap.enabled = true
 
-function headani(){
-    
-}
-
-
-
-
 function animate(){
 
     var timer = Date.now() * 0.0001;
     headspin()
 
     //raycaster.setFromCamera(mouse, camera2)
-
    // const intersects = raycaster.intersectObject(box2mesh)
-
     /*const objectToTest = []
-
 
     for(const object of objectToTest){
        object.material.color.set('white')
@@ -312,16 +185,7 @@ function animate(){
        intersect.object.material.color.set('red')
     }*/
     
-
-
     requestAnimationFrame(animate)
-
-
-   //controls.update();
     renderer.render(scene, camera)
-
-
 }
-    
-
 animate()
