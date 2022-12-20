31b44b47eb70efdb66af6640eb924a714bf9f34e
personthe <84892109+personthe@users.noreply.github.com>
2022-12-19 23:11:32 -0500

    Add files via upload

--- mobileScript.js
+++ mobileScript.js
@@ -245,7 +245,7 @@ const textMat = new THREE.MeshBasicMaterial({color: 'white', wireframe: true})
    
 let text;
 fontLoader.load('/font/helvetiker_regular.typeface.json',(font) =>{
-    const textgeo = new THREE.TextGeometry('textString',{
+    const textgeo = new THREE.TextGeometry('testing',{
         font: font,
         size: 0.6,
         height: 0.2,
@@ -472,7 +472,7 @@ window.addEventListener('touchend',  (event) => {
 
     setTimeout(function() {
         window.dispatchEvent(clickEvent);
-      }, 1)
+      }, 100)
     })
 
     window.addEventListener('mousedown', (event) =>{
